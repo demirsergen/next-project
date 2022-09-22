0735318e839ddfaecbedfc9e2a63d216b919ba7c
sergen demir <dmrsrgn@gmail.com>
2022-09-22 18:11:31 -0500

    display results after search

--- components/results.jsx
+++ components/results.jsx
@@ -1,7 +1,32 @@
 import React from "react";
+import Image from "next/image";
+import Link from "next/link";
 
 const Results = ({ results }) => {
-  return <div></div>;
+  console.log(results);
+  return (
+    <div className="container flex flex-wrap align-center justify-between">
+      {results.map((result) => (
+        <div key={result.property_id} className="p-2 mx-auto border-2 w-64">
+          <Image
+            src={result.photos ? result.photos[0].href : result.thumbnail}
+            alt="home-image"
+            width={200}
+            height={200}
+          />
+          <h4>
+            {result.prop_status === "for_sale" ? "$" + result.price : null}
+          </h4>
+          <a href={result.rdc_web_url} target="_blank" rel="noreferrer">
+            Check out property
+          </a>
+
+          <p>{`${result.beds} beds ${result.baths} baths`}</p>
+          <p>{result.address.line}</p>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Results;
--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: ["ar.rdcpix.com", "ap.rdcpix.com"],
+  },
 };
 
 module.exports = nextConfig;
